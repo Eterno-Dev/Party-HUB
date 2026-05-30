6a6344561e961c858ddf4fc93b50d527e1793cd0
paucg06 <paucremades06@gmail.com>
2026-05-30 06:10:18 +0200

    Fix routing for GH Pages and Localhost

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { GameProvider } from './contexts/GameContext';
 import Home from './pages/Home';
 import Lobby from './pages/Lobby';
